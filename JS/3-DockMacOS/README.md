0d1e6c93dfd06b3ee7eb4a6a9872da743391bf58
Jonathan <contact@grafikart.fr>
2021-10-20 20:50:05 +0200

    feat: exercice dock macos

---
+++ JS/3-DockMacOS/README.md
@@ -0,0 +1,5 @@
+# Recréer le dock de MacOS
+
+Tout est dans le titre ;)
+
+La solution se trouver dans le dossier SRC
---
+++ JS/3-DockMacOS/src/main.ts
@@ -0,0 +1,110 @@
+import './style.css'
+
+function scaling (x: number) {
+  return Math.min(
+    Math.max(
+      -0.2 * Math.pow(x, 2) + 1.05,
+      0
+    ),
+    1
+  )
+}
+
+enum Direction {
+  Left = 'right',
+  Right = 'left',
+  Up = 'bottom',
+  Down = 'top',
+  None = 'center',
+}
+
+enum Position {
+  Top = 'top',
+  Bottom = 'bottom',
+  Left = 'left',
+  Right = 'right'
+}
+
+class MacDock {
+
+  private root: HTMLElement
+  private icons: HTMLElement[]
+  private iconSize: number
+  private mousePosition: number = 0
+  private scale: number = 0.5
+  private position: Position
+
+  constructor (el: HTMLElement, { position = Position.Left }: { position: Position }) {
+    this.root = el
+    this.position = position
+    this.icons = Array.from(el.children) as HTMLElement[]
+    this.iconSize = this.icons[0].offsetWidth
+    el.addEventListener('mousemove', this.handleMouseMove.bind(this))
+    el.addEventListener('mouseenter', this.handleMouseEnter.bind(this))
+    el.addEventListener('mouseleave', this.handleMouseLeave.bind(this))
+  }
+
+  get isVertical () {
+    return [Position.Left, Position.Right].includes(this.position)
+  }
+
+  handleMouseMove (e: MouseEvent) {
+    if (this.isVertical) {
+      this.mousePosition = (e.clientY - this.root.offsetTop) / this.iconSize
+    } else {
+      this.mousePosition = (e.clientX - this.root.offsetLeft) / this.iconSize
+    }
+    this.scaleIcons()
+  }
+
+  scaleIcons () {
+    const selectedIndex = Math.floor(Math.abs(this.mousePosition));
+    const centerOffset = (this.mousePosition - selectedIndex - 0.5)
+    const baseOffset = this.scaleFromDirection(selectedIndex, Direction.None, -centerOffset * this.iconSize * this.scale);
+    let offset = baseOffset * (0.5 - centerOffset)
+    for (let i = selectedIndex + 1; i < this.icons.length; i++) {
+      offset += this.scaleFromDirection(i, this.isVertical ? Direction.Down : Direction.Right, offset);
+    }
+    this.root.style.setProperty('--after', `${offset}px`)
+    offset = baseOffset * (0.5 + centerOffset)
+    for (let i = selectedIndex - 1; i >= 0; i--) {
+      offset += this.scaleFromDirection(i, this.isVertical ? Direction.Up : Direction.Left, -offset);
+    }
+    this.root.style.setProperty('--before', `${offset}px`)
+  }
+
+  scaleFromDirection (index: number, direction: Direction, offset: number): number {
+    const icon = this.icons[index]
+    const x = this.mousePosition - index - 0.5
+    const scale = scaling(x) * this.scale;
+    icon.style.setProperty('transform', `translate${this.isVertical ? 'Y' : 'X'}(${offset}px) scale(${scale + 1})`)
+    icon.style.setProperty('transform-origin', `${direction} ${this.position}`)
+    return scale * this.iconSize
+  }
+
+  handleMouseLeave () {
+    this.icons.forEach((icon) => {
+      icon.style.removeProperty('transform')
+      icon.style.setProperty('transition', 'transform .1s');
+    })
+    this.root.style.removeProperty('--after')
+    this.root.style.removeProperty('--before')
+  }
+
+  handleMouseEnter () {
+    this.icons.forEach((icon) => {
+      icon.style.setProperty('transition', 'transform .1s');
+    })
+    window.setTimeout(() => {
+      this.icons.forEach((icon) => {
+        icon.style.removeProperty('transition')
+      })
+    }, 100)
+  }
+
+}
+
+new MacDock(document.querySelector('.dock__wrapper--bottom .dock')!, { position: Position.Bottom })
+new MacDock(document.querySelector('.dock__wrapper--top .dock')!, { position: Position.Top })
+new MacDock(document.querySelector('.dock__wrapper--left .dock')!, { position: Position.Left })
+new MacDock(document.querySelector('.dock__wrapper--right .dock')!, { position: Position.Right })
